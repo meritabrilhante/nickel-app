b426ce5564f72b1def3f985aa00388c84fa53be1
Rita Brilhante <meritabrilhante@gmail.com>
2023-04-28 20:34:16 -0300

    Create input component

--- src/app/presentation/components/layout/atoms/input/Input.component.tsx
+++ src/app/presentation/components/layout/atoms/input/Input.component.tsx
@@ -1,26 +1,53 @@
-import { Box, IconButton, InputAdornment, InputBase, InputLabel, styled } from '@mui/material';
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  InputBase,
+  InputLabel,
+  Stack,
+  styled,
+} from "@mui/material"
 
-const InputContainer = styled(Box)(({ height }: InputProps) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
+const InputContainer = styled(Box)(({ height }: InputContainerProps) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
   height: height,
-}));
+}))
 
-interface InputProps {
+interface InputContainerProps {
   height: string
 }
 
+interface InputProps {
+  height?: string
+  children?: React.ReactNode
+  label?: string
+  message?: string
+  placeholder?: string
+  width?: string
+}
 
-const Input = ({ height }: InputProps) => {
-  return(
-    <InputContainer height={height}>
-      <InputBase
-        type='text'
-        placeholder={'Digite algo aqui...'}
-      ></InputBase>
+const Input = ({ children, label, message, placeholder }: InputProps) => {
+  return (
+    <InputContainer height={"40px"}>
+      <Stack spacing={1}>
+        <InputLabel>{label} Label</InputLabel>
+
+        <InputBase
+          type="text"
+          placeholder={placeholder}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton>{children}</IconButton>
+            </InputAdornment>
+          }
+        ></InputBase>
+
+        <p style={{ fontSize: "0.875rem" }}>{message} Messagem</p>
+      </Stack>
     </InputContainer>
-  );
-};
+  )
+}
 
-export default Input;
+export default Input
