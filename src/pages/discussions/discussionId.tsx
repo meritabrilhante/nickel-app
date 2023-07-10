3f72f2395f71b030fc72a6dbd65d3550031a4775
Rita Brilhante <meritabrilhante@gmail.com>
2023-07-10 18:19:06 -0300

    <chore> help

--- src/pages/discussions/discussionId.tsx
+++ src/pages/discussions/discussionId.tsx
@@ -1,17 +1,25 @@
 import MainLayout from "@/app/presentation/components/layout/main-layout/MainLayout.component";
 import { Box, Stack, styled } from "@mui/material";
 import { Breadcrumbs } from "@/app/presentation/components/layout/atoms/breadcrumbs";
-import { ContentCard } from "@/app/presentation/components/layout/atoms/content-card";
 import { Button } from "@/app/presentation/components/layout/atoms/button";
 import { TextIcon } from "@/app/presentation/components/layout/atoms/text-icon";
 import { TitleHeader } from "@/app/presentation/components/layout/atoms/title-header";
 import Answer from "@/app/presentation/components/layout/atoms/answer/Answer.component";
+import DiscussionCard from "@/app/presentation/components/layout/atoms/content-card/DiscussionCard.component";
+import { Badge } from "@/app/presentation/components/layout/atoms/badge";
 
 const DiscussionContainer = styled(Box)(() => ({
   display: "flex",
+  padding: "32px 0",
   flexDirection: "column",
-  alignItems: "center",
   height: "100vh",
+  gap: "40px",
+}));
+
+const TitleContainer = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 }));
 
 export const Discussion = () => {
@@ -29,23 +37,27 @@ export const Discussion = () => {
       mainButtonAction={"/answers/new"}
     >
       <DiscussionContainer>
-        <Stack
-          spacing={4}
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            width: "100%",
-            alignItems: "center",
-          }}
-        >
+        <Stack spacing={2}>
           <Breadcrumbs path={breadcrumbsPath} />
+          <TitleContainer>
+            <h1>Discussão</h1>
 
-          <Stack>
-            <TitleHeader title={"Título da Discussão"} description={"0x8B5F2A"} />
-          </Stack>
+            <p>#123456</p>
+          </TitleContainer>
         </Stack>
 
-        <ContentCard type={"discussion"} />
+        <DiscussionCard
+          discussionTitle={"Qual é o principal mistério presente no enredo de Dom Casmurro?"}
+          discussionId={"#123456"}
+          description={"Como ele influencia a narrativa?"}
+          badge={
+            <Badge mainColor={"#5FEE98"} borderRadius={"9999px"} height={"32px"}>
+              <TextIcon iconName={"FiBook"} text={"Literatura Brasileira"} iconPosition={"left"} />
+            </Badge>
+          }
+          author={"Machado de Assis"}
+          bookName={"Dom Casmurro"}
+        />
 
         <Stack
           style={{
@@ -53,17 +65,20 @@ export const Discussion = () => {
             flexDirection: "row",
             justifyContent: "space-between",
             width: "100%",
-            paddingBottom: "32px",
           }}
         >
-          <h2 style={{ marginRight: "300px" }}>Respostas</h2>
+          <h2>Respostas</h2>
 
-          <Button buttonClass={"tertiary"} mainColor={""}>
+          <Button buttonClass={"tertiary"} width={"200px"}>
             <TextIcon iconName={"FiFilter"} text={"Mais Relevante"} iconPosition={"left"} />
           </Button>
         </Stack>
 
-        <Answer />
+        <Answer
+          answer={
+            "O principal mistério presente no enredo é se o protagonista, Bento Santiago (ou Bentinho), foi traído por sua esposa, Capitu, com seu melhor amigo, Escobar. Essa suspeita de traição permeia a narrativa e gera uma grande dúvida em relação à fidelidade de Capitu e à verdade dos fatos. Ao longo da história, Machado de Assis utiliza recursos narrativos habilidosos para deixar o leitor em suspense e criar ambiguidades em relação aos eventos. A incerteza sobre a traição de Capitu influencia a forma como interpretamos os personagens e suas ações ao longo do livro, adicionando camadas de complexidade emocional e psicológica"
+          }
+        />
       </DiscussionContainer>
     </MainLayout>
   );
